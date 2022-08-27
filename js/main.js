38358212b444c5263b666dafa0ef3c9cb70164df
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-08-27 11:01:34 -0400

    fixed errors

--- js/main.js
+++ js/main.js
@@ -77,3 +77,23 @@ fetch(finalURL)
         return `${api}username=${username}`
     }
 
+
+    //gallery
+
+    $(document).ready(function(){
+        $("img").click(function(){
+        let t = $(this).attr("src");
+        $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+        $("#myModal").modal();
+      });
+      
+      $("video").click(function(){
+        let v = $("video > source");
+        let t = v.attr("src");
+        $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+        $("#myModal").modal();  
+      });
+      });//EOF Document.ready
+    
+    
+    
